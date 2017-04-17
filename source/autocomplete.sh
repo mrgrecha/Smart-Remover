c396c605caca0234ecc4b8cba63b99d5bd22db9d
Dima <dimon2810@tut.by>
2017-04-17 22:22:16 +0300

    Added autocomplete

---
+++ source/autocomplete.sh
@@ -0,0 +1,34 @@
+if [[ $ZSH_NAME == "zsh" ]]; then
+    autoload bashcompinit
+    bashcompinit
+fi
+
+_my_autocomple()
+{
+    local cur
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+
+    subcommands_1="delete clear list full recover"
+
+    if [[ ${COMP_CWORD} == 1 ]] ; then
+        COMPREPLY=( $(compgen -W "${subcommands_1}" -- ${cur}) )
+        return 0
+    fi
+
+    subcmd_1="${COMP_WORDS[1]}"
+
+    case "${subcmd_1}" in
+    delete )
+        COMPREPLY=( $(compgen -W "`ls ${HOME}/.MyTrash`" -- ${cur}) )
+        return 0 ;;
+
+    recover )
+        COMPREPLY=( $(compgen -W "`ls ${HOME}/.MyTrash`" -- ${cur}) )
+        return 0 ;;
+
+    esac
+    return 0
+}
+
+complete -F _my_autocomplete trash
